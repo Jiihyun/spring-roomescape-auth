4d243ba8a7dcf53f76a7d2f0557d523888b6a1cf
Jiihyun <cwlgusc@gmail.com>
2026-05-24 19:39:29 +0900

    fix: 인가 처리 필요한 경로 수정

--- src/main/java/roomescape/config/WebConfig.java
+++ src/main/java/roomescape/config/WebConfig.java
@@ -27,6 +27,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(adminAuthInterceptor)
-                .addPathPatterns("/admin");
+                .addPathPatterns("/admin/**");
     }
 }
